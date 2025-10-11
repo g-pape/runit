g-pape/runit

commit 5fe1bc773c2d979093fe4b1f3ecbbed5e6acdaf0

* sv.c: properly format status command's output on failure cases.

src/sv.c
@@ -41,6 +41,7 @@ unsigned int lsb;
 unsigned int verbose =0;
 unsigned long wait =7;
 unsigned int kll =0;
+unsigned int islog =0;
 struct taia tstart, tnow, tdiff;
 struct tai tstatus;
 
@@ -67,6 +68,7 @@ void fatal2(char *m1, char *m2) {
 void out(char *p, char *m1) {
   buffer_puts(buffer_1, p);
   buffer_puts(buffer_1, *service);
+  if (islog) buffer_puts(buffer_1, "/log");
   buffer_puts(buffer_1, ": ");
   buffer_puts(buffer_1, m1);
   if (errno) {
@@ -155,18 +157,20 @@ int status(char *unused) {
   rc =svstatus_get();
   switch(rc) { case -1: if (lsb) done(4); case 0: return(0); }
   rc =svstatus_print(*service);
+  islog =1;
   if (chdir("log") == -1) {
     if (errno != error_noent) {
-      outs("; log: "); outs(WARN);
-      outs("unable to change to log service directory: ");
-      outs(error_str(errno));
+      outs("; ");
+      warn("unable to change directory");
     }
+    else outs("\n");
   }
-  else
-    if (svstatus_get()) {
-      outs("; "); svstatus_print("log");
-    }
-  flush("\n");
+  else {
+    outs("; ");
+    if (svstatus_get()) { rc =svstatus_print("log"); outs("\n"); }
+  }
+  islog =0;
+  flush("");
   if (lsb) switch(rc) { case 1: done(0); case 2: done(3); case 0: done(4); }
   return(rc);
 }

src/sv.dist
@@ -1,7 +1,7 @@
 usage: sv [-v] [-w sec] command service ...
 
 100
-$Id: 7d3ba19cde56692911cf4b2d711774583b1b860f $
+$Id: 900314260c6d52c986c5357673bea2f3bd3f4698 $
 usage: sv [-v] [-w sec] command service ...
 
 100

commit d8f338ba5b26b20780c704109565bb1af4ef7256

* sv.c: fix regression introduced with 5fe1bc7: rc of sv status in lsb mode
Starting with 5fe1bc7 the exit code of sv status in "lsb" mode was not 3 in
all cases when the service was "down" (thx Fabian Ruff, David Mountney for
the patch).

This commit finally applies the patch from David Mountney, see
https://skarnet.org/lists/supervision/index.html#msg939

src/sv.c
@@ -168,7 +168,7 @@ int status(char *unused) {
   }
   else {
     outs("; ");
-    if (svstatus_get()) { rc =svstatus_print("log"); outs("\n"); }
+    if (svstatus_get()) { svstatus_print("log"); outs("\n"); }
   }
   islog =0;
   flush("");

src/sv.dist
@@ -1,7 +1,7 @@
 usage: sv [-v] [-w sec] command service ...
 
 100
-$Id: bccdf71babe2596029c67cd0f6cec89bd05844a2 $
+$Id: baa96cd95da0d4daef7818741632c7b5e5d34a05 $
 usage: sv [-v] [-w sec] command service ...
 
 100