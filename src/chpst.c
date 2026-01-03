7e4a3ed810afc0637f328cc5eec72d1c7fcf1a4d
G. Pape <pape@smarden.org>
2026-01-03 11:47:47 +0100

    chpst.c: -F: pid1: only check /proc after getting ECHILD
    Instead of looking in /proc whenever a child exits, only check /proc
    after waitpid() reports ECHILD, which basically means new pid 1 is now
    alone. If there's still other processes listed in /proc though, sleep
    a few seconds before exiting and so forcefully killing those ghosts.

--- src/chpst.c
+++ src/chpst.c
@@ -281,7 +281,7 @@ void sig_handler(int sig) { kill(pid, sig); }
 void sig_handler_pid1(int sig) { kill(-1, sig); }
 void sig_quit_handler(int unused) { kill(pid, SIGKILL); }
 void newpid1() {
-  int i;
+  int i, w =0;
   DIR *dir;
   direntry *d;
   unsigned int pids =0;
@@ -317,26 +317,38 @@ void newpid1() {
       for (;;) {
         pid_t p;
         if ((p =wait_pid(&i, -1)) == -1) {
-          strerr_warn2(WARNING, "pid1: wait_pid(): ", &strerr_sys);
-        } else
+          if (errno == ECHILD) { /* iamalone? */
+            if (!(dir =opendir("/proc")))
+              fatal("pid1: unable to open directory: /proc");
+            for (pids =0;;) {
+              errno =0;
+              if (!(d =readdir(dir))) {
+                if (errno) fatal("pid1: unable to read directory: /proc");
+                break;
+              }
+              if (('0' < d->d_name[0]) && (d->d_name[0] <= '9')) ++pids;
+            }
+            if (closedir(dir) == -1)
+              fatal("pid1: unable to close directory: /proc");
+            if (pids > 1) {
+              bufnum[fmt_ulong(bufnum, --pids)] =0;
+              strerr_warn5(WARNING, "pid1: no more zombies, but ", bufnum,
+                           pids == 1 ? " ghost" : " ghosts", ": waiting", 0);
+              sleep(7);
+              strerr_warn3(WARNING, "pid1: ignoring",
+                           pids == 1 ? " ghost" : " ghosts", 0);
+            }
+            _exit(wait_crashed(w) ? 128 + WTERMSIG(w) : wait_exitcode(w));
+          }
+          else
+            strerr_warn2(WARNING, "pid1: wait_pid(): ", &strerr_sys);
+        } else {
+          w =i;
           if (verbose) {
             bufnum[fmt_ulong(bufnum, p)] =0;
             strerr_warn4(WARNING, "pid1: pid ", bufnum, ": exit", 0);
           }
-        if (!(dir =opendir("/proc")))
-          fatal("pid1: unable to open directory: /proc");
-        for (pids =0; pids <= 1;) {
-          errno =0;
-          if (!(d =readdir(dir))) {
-            if (errno) fatal("pid1: unable to read directory: /proc");
-            break;
-          }
-          if (('0' < d->d_name[0]) && (d->d_name[0] <= '9')) ++pids;
         }
-        if (closedir(dir) == -1)
-          fatal("pid1: unable to close directory: /proc");
-        if (pids <= 1)
-          _exit(wait_crashed(i) ? 128 + WTERMSIG(i) : wait_exitcode(i));
       }
     /* pid 2 */
   }
