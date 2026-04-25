6e606222e64ef91482c0ae9087e7972d316533ac
G. Pape <pape@smarden.org>
2026-04-25 11:35:13 +0200

    src: sv status: confusing output if log/supervise/ cannot be accessed
    Don't print confusing additional line after warning message (#36);
    introduced with 5fe1bc7.

--- src/sv.c
+++ src/sv.c
@@ -168,7 +168,7 @@ int status(char *unused) {
   }
   else {
     outs("; ");
-    if (svstatus_get()) { svstatus_print("log"); outs("\n"); }
+    if (svstatus_get() > 0) { svstatus_print("log"); outs("\n"); }
   }
   islog =0;
   flush("");
